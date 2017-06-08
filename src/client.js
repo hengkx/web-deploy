9d763c0437f1cac27a40f4c773bbb35314e9f756
hengkx <ycxzhkx@gmail.com>
2017-06-08 20:27:09 +0800

    add progress

--- src/client.js
+++ src/client.js
@@ -5,37 +5,33 @@ var fs = require('fs');
 var ss = require('socket.io-stream');
 var adm_zip = require('adm-zip');
 
+var socket = io.connect('http://localhost:9000');
 
-fs.readdir(process.cwd(), function (err, files) {
-    var zip = new adm_zip();
-    files.forEach((item) => {
-        const filePath = path.join(__dirname, item);
-        const stat = fs.statSync(filePath);
-        if (stat.isDirectory()) {
-            zip.addLocalFolder(item);
-        } else {
-            console.log(filePath);
-            var input = fs.readFileSync(filePath);
-            zip.addFile(item, input, './s', 0644);
-            // zip.addLocalFile(filePath);
-        }
+socket.on('connect', function () {
+    console.log('connect');
+
+    var stream = ss.createStream();
+    var filename = 'C:/Users/happy/Desktop/NovelCrawler.zip';
+    const size = fs.statSync(filename).size;
+    console.log(size);
+
+    var uploadedSize = 0;
+
+    stream.on('end', function () {
+        console.log('clent end');
+        // var unzip = new adm_zip(filename);
+        // unzip.extractAllTo(".", /*overwrite*/true);
     });
-    // zip.addLocalFolder('H:/BaiduNetdiskDownload');
-    console.log('object');
-    var willSendthis = zip.toBuffer();
-    zip.writeZip('adm-archive.zip');
+
+    ss(socket).emit('deploy', stream, { name: filename, size, key: 'test' });
+
+    fs.createReadStream(filename).pipe(stream);
+
+});
+socket.on('disconnect', function () {
+    console.log('disconnect');
 });
 
-
-var socket = io.connect('http://localhost:9000');
-var stream = ss.createStream();
-
-var filename = 'C:/Users/happy/Desktop/NovelCrawler.zip';
-
-ss(socket).emit('deploy', stream, { name: filename, key: 'test' });
-
-fs.createReadStream(filename).pipe(stream);
-stream.on('end', function () {
-    var unzip = new adm_zip(filename);
-    unzip.extractAllTo(".", /*overwrite*/true);
+socket.on('progress', (item) => {
+    console.log(item);
 });
--- src/server.js
+++ src/server.js
@@ -9,11 +9,27 @@ var adm_zip = require('adm-zip');
 io.on('connection', function (client) {
     ss(client).on('deploy', function (stream, data) {
         var filename = path.basename(data.name);
+        // console.log(path.parse(data.name).name);
         console.log(filename);
-        console.log(data.key);
-        
+        console.log(data);
 
         stream.pipe(fs.createWriteStream(filename));
+
+        const { size, key } = data;
+        console.log(size);
+
+        var uploadedSize = 0;
+
+        stream.on('data', function (buffer) {
+            var segmentLength = buffer.length;
+
+            // Increment the uploaded data counter
+            uploadedSize += segmentLength;
+            client.emit('progress', (uploadedSize / size * 100).toFixed(2));
+            // Display the upload percentage
+            // console.log("Progress:\t", ((uploadedSize / size * 100).toFixed(2) + "%"));
+        });
+
         stream.on('end', function () {
             var unzip = new adm_zip(filename);
             unzip.extractAllTo(".", /*overwrite*/true);
